3c97d20ea2e4fc4f872dbedcdf7ceb8f878d7d13
Bohdan <bohdan@MacBook-Pro-Bohdan.local>
2023-05-17 13:54:48 +0300

    fix next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
-module.exports = {
-    output: {
-      export: true,
-    },
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+    // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+    // trailingSlash: true,
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
   };
+   
+  module.exports = nextConfig;
